ccadb213e8c65af170c687d25cc818409357ebbb
Axel358 <edgar.serrano.9803@gmail.com>
2022-07-03 14:28:57 -0400

    Add verbose name for chief
    Implement __str__ method for models

--- server_project/server/models.py
+++ server_project/server/models.py
@@ -6,10 +6,16 @@ class Chief(User):
     name = models.TextField()
     is_program_chief = models.BooleanField()
 
+    class Meta():
+        verbose_name = 'Chief'
+
 
 class Program(models.Model):
     name = models.TextField(default='')
     chief = models.OneToOneField(Chief, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Project(models.Model):
@@ -23,8 +29,14 @@ class Project(models.Model):
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
     chief = models.ForeignKey(Chief, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class Document(models.Model):
     name = models.TextField()
     path = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
